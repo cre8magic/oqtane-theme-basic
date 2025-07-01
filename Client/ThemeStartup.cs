2bd40153fd25f54d71d057a75d91a753f6d1127b
iJungleboy <daniel.mettler@2sic.com>
2025-07-01 17:44:38 +0200

    minor docs

--- Client/ThemeStartup.cs
+++ Client/ThemeStartup.cs
@@ -2,11 +2,6 @@
 
 namespace ToSic.Cre8magic.Theme.Basic;
 
-// TODO: @2dg
-// - document how the DI works
-//   - Document how general DI works in themes
-//   - Document in the docs of this theme, that it uses DI
-
 public class ThemeStartup : Oqtane.Services.IClientStartup
 {
     /// <summary>
